82e33315b77abc2c3e4bf2b488acf0399337d2af
superLam <superLam>
2018-06-12 15:12:22 +0800

    第三次加分题

---
+++ exam3/exam1.py
@@ -0,0 +1,21 @@
+# coding=utf-8
+
+#计算鸡的数量
+
+print('i will now count my chickens: ')
+print('Hens ', 25 + 30 / 6)
+print('Roosters ', 100 - 25 * 3 % 4 )
+#计算蛋的数量
+print('Now i will count the eggs: ')
+print('%.2f' %(3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6))  #即使设置成保留两位小数，可是还是会四舍五入
+print('is it true that 3 + 2 < 5 - 7 ?')
+print(3 + 2 < 5 - 7)
+#当print的内容不是全部在引号里面，输出时就会出现括号。
+print('what is 3 + 2 ? ', 3 + 2)
+print('what is 5 - 7 ? ', 5 - 7)
+print("Oh, that's why it's False.")
+print('How about some more.')
+#这里会直接判断true或者false。
+print('Is it greater ? ' , 5 > -2 )
+print('Is it greater or equal?' , 5 >= -2)
+print('Is it less or equal?', 5 <= -2)
---
+++ exam3/exam2.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+# 首项加末项的和，乘以项数，除以二
+print('计算连加的计算器')
+x = input('x = ')
+y = input('y = ')
+z = (x + y)*(y - x + 1)/2
+print(z)
