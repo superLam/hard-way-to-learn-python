6727fc2cd74fbe10c4fb9c0fd2211ac7b3d25497
Lamgor <513968746@qq.com>
2018-06-12 15:22:24 +0800

    Update exam1.py

--- exam2/exam1.py
+++ exam2/exam1.py
@@ -1,3 +1,9 @@
+#2. 打开你的 ex2.py 文件，从后往前逐行检查。从最后一行开始，倒着逐个单词单词检查回去。
+#不知道有什么错，注释拼写错误？
+ 
+#3. 有没有发现什么错误呢？有的话就改正过来.
+
+
 #A comment, this is so you can read your program Later.
 #Anything after the # is ignored by python
 
