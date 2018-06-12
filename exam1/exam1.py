90c30cfc7e4cdcfeeb627ae7c83fc8332f5101ec
superLam <superLam>
2018-06-12 14:16:01 +0800

    第一加分题

---
+++ exam1/exam1.py
@@ -0,0 +1,5 @@
+print('hello world')
+print('hello again')
+print('i like typing this')
+print('\n')
+print('this is fun')
---
+++ exam1/exam2.py
@@ -0,0 +1,1 @@
+print('hello world','hello again','i like typing this','this is fun')
