8310bc9d607c56591ac5ab59bb4da57febda2b85
superLam <513968746@qq.com>
2018-06-19 17:10:10 +0800

    巩固练习12

---
+++ exam12/exam1.py
@@ -0,0 +1,7 @@
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh ")
+
+# 两个得到的结果是一致。
+print(f"So, you're {age} old , {height} tall and {weight} heavy.")
+print("So, you're {} old, {} tall and {} heavy.".format(age, height, weight))
